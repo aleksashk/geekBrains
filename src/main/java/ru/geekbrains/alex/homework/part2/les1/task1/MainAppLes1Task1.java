f64d6340a1c9f6b06ba5dd86b7ad440dda8f669b
Aleksandr <aleksandrphilimonov@gmail.com>
2021-02-11 22:13:29 +0300

    создал папку для дз по первой лекции

---
+++ src/main/java/ru/geekbrains/alex/homework/part2/les1/task1/MainAppLes1Task1.java
@@ -0,0 +1,4 @@
+package ru.geekbrains.alex.homework.part2.les1.task1;
+
+public class MainAppLes1Task1 {
+}
---
+++ src/main/java/ru/geekbrains/alex/homework/part2/les1/task2/MainAppLes1Task2.java
@@ -0,0 +1,4 @@
+package ru.geekbrains.alex.homework.part2.les1.task2;
+
+public class MainAppLes1Task2 {
+}
