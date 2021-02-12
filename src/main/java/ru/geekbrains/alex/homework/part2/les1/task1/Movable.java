22fdca37a055ba62c2cd67c768044034964954dd
Aleksandr <aleksandrphilimonov@gmail.com>
2021-02-12 15:50:37 +0300

    очистил task1 les1 part2

--- src/main/java/ru/geekbrains/alex/homework/part2/les1/task1/Movable.java
+++
@@ -1,5 +0,0 @@
-package ru.geekbrains.alex.homework.part2.les1.task1;
-
-public interface Movable {
-    void move();
-}
--- src/main/java/ru/geekbrains/alex/homework/part2/les1/task2/MainAppLes1Task2.java
+++
@@ -1,4 +0,0 @@
-package ru.geekbrains.alex.homework.part2.les1.task2;
-
-public class MainAppLes1Task2 {
-}
