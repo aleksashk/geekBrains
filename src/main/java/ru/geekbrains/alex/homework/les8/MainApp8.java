aa5a014b02102a62197f309aad2096c13d387d76
Aleksandr <aleksandrphilimonov@gmail.com>
2021-03-03 11:04:38 +0300

    добавил 5 дз

--- src/main/java/ru/geekbrains/alex/homework/les8/MainApp8.java
+++
@@ -1,7 +0,0 @@
-package ru.geekbrains.alex.homework.les8;
-
-public class MainApp8 {
-    public static void main(String[] args) {
-        GameWindow gw = new GameWindow();
-    }
-}
---
+++ src/main/java/ru/geekbrains/alex/homework/part2/les5/MainAppDZ5.java
@@ -0,0 +1,10 @@
+package ru.geekbrains.alex.homework.part2.les5;
+
+public class MainAppDZ5 {
+    public static void main(String[] args) throws InterruptedException {
+        SomeClass someClass = new SomeClass();
+
+        someClass.fillArrayFirstMeth();
+        someClass.fillArraySecondMeth();
+    }
+}
