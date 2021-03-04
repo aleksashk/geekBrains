10034308022c1861e175bd36909ccf9cca33ef48
Aleksandr <aleksandrphilimonov@gmail.com>
2021-03-04 20:58:08 +0300

    исправил ошибку в расчетах

--- src/main/java/ru/geekbrains/alex/homework/part2/les5/SomeClass.java
+++ src/main/java/ru/geekbrains/alex/homework/part2/les5/SomeClass.java
@@ -1,7 +1,7 @@
 package ru.geekbrains.alex.homework.part2.les5;
 
 public class SomeClass {
-    static final int SIZE = 10000000;
+    static final int SIZE = 50_000_000;
     static final int HALF = SIZE / 2;
     float[] arr = new float[SIZE];
 
@@ -26,27 +26,27 @@ public class SomeClass {
         System.arraycopy(arr, 0, arrFirstPart, 0, HALF);
         System.arraycopy(arr, HALF, arrSecondPart, 0, HALF);
 
-        long a = System.currentTimeMillis();
+        long b = System.currentTimeMillis();
 
-        Thread t1 = new Thread(() -> {
+        Thread t3 = new Thread(() -> {
             for (int i = 0; i < HALF; i++) {
                 arrFirstPart[i] = (float) (arrFirstPart[i] * Math.sin(0.2 + i / 5.) * Math.cos(0.2 + i / 5.) * Math.cos(0.4 + i / 2.));
             }
         });
-        Thread t2 = new Thread(() -> {
-            for (int i = 0; i < HALF; i++) {
-                arrSecondPart[i] = (float) (arrSecondPart[i] * Math.sin(0.2 + i / 5.) * Math.cos(0.2 + i / 5.) * Math.cos(0.4 + i / 2.));
+        Thread t4 = new Thread(() -> {
+            for (int i = 0, j = HALF; i < HALF; i++, j++) {
+                arrSecondPart[i] = (float) (arrSecondPart[i] * Math.sin(0.2 + j / 5.) * Math.cos(0.2 + j / 5.) * Math.cos(0.4 + j / 2.));
             }
         });
 
-        t1.start();
-        t2.start();
-        t1.join();
-        t2.join();
+        t3.start();
+        t4.start();
+        t3.join();
+        t4.join();
 
         System.arraycopy(arrFirstPart, 0, arr, 0, HALF);
         System.arraycopy(arrSecondPart, 0, arr, HALF, HALF);
-        System.out.println("time for working of the fillArraySecondMeth: " + (System.currentTimeMillis() - a));
+        System.out.println("time for working of the fillArraySecondMeth: " + (System.currentTimeMillis() - b));
 
     }
 }
